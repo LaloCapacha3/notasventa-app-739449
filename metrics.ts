f2eddc39ffafc5f999d19c84288f7d8b0ac709b7
LaloCapacha3 <donapeluda@hotmail.com>
2025-05-05 20:51:43 -0600

    1

--- metrics.ts
+++ metrics.ts
@@ -3,9 +3,7 @@ import os from 'os';
 
 // Configurar cliente CloudWatch
 const cloudWatch = new AWS.CloudWatch({
-  region: process.env.AWS_REGION || 'us-east-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: process.env.AWS_REGION || 'us-east-1'
 });
 
 // Ambiente para etiquetar las métricas
