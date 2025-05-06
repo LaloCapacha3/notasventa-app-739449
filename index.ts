36f7469ad38100e06a8560217d89e2b49f149269
LaloCapacha3 <donapeluda@hotmail.com>
2025-05-05 20:41:59 -0600

    1

--- index.ts
+++ index.ts
@@ -11,11 +11,7 @@ import fetch from 'node-fetch';
 dotenv.config();
 
 // Configuración de AWS
-AWS.config.update({
-  region: process.env.AWS_REGION || 'us-east-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
